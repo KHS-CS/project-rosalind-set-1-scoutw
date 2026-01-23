54e8827d707300f131f861d33e8cf62af9acc83b
scoutw <scoutwoodcock@gmail.com>
2026-01-23 11:22:25 -0500

    Update ProblemREVC.java

--- REVC/ProblemREVC.java
+++ REVC/ProblemREVC.java
@@ -1,24 +1,25 @@
-
-/**
- * Write a description of class ProblemRNA here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class ProblemREVC {
     public static void main() {
-        // replace this string with the one from Project Rosalind
-        final String inputDNA = "AAAACCCGGT";
+        final String inputDNA = "GAAGCCTATCGGACGGTCCATCTTTATGTGTGTCGGCCGTCCCATGCGTAAGTTATTGACTTCGACGGATTACATACTAGACCTGATACAGCAGTGCGACCCAATATCCGTGGATTTGACATGGGGAATCTGGTAACTGTAGCTAACAGTGAGACCGAGCCCTGAGGCTGGACGTGCTGCCGACATCCAGGCCATATAGAGGGTTTAGTCAGGTCGCCGATTGTTTCGAGTGCGCAACCGTTGTTTCGATGGCAGGCAACGCTTGTTCTGGAGGAAATTAGATCCGTGTCGAATGCCACGAGGTGCTATGTGTGTTTCGGTGTCCTCCGCGTTAGTATATATCCCATCCTTTAGAATTGTGGTAATCTCACCACGTAGGCAGCTGGGCGGAGACCTAGCCCTGGGCGAGTTCATGATACGAGCTTGCTTTCAGAATTCGTAAGCTGAGCCAGCTTAGGCCGCTCTAACCAACACCGGTACGGCTTTCGCCTACAACAGCAGTAACACGTGACGCCCGTGCAATGTCGAATGCGCTACGTGCTGTAGGATAAAAAGCGCTAGGATTCGAATGGGTCTGGAGGCATTCGGCCAGGCCATTACAAATGTCCACAGCTCGAATGGCTGTTGGGGGCATTCTGGAATCCGCGTCCAGGCCCGCGTAGAATACGAACCGCTAGTATCTGATGCGGTACTGTCAGGGCGTTTTGAGGCACAGAATAGGAGCACGTGTCGAGGCCTTCTACTATTCACATAGTGACCGGAGGCTATTTTTCAAATGCCCCACGGCGCGCATACAGTCAAGATAGGCAGAACCCTACGTCTGAGGGGCCTCTAGAAAGCGCTGACCAAAAACGCAGATATTATAGGTTTCTGATCCTGAAGTTCCTCCGTAGAACTCTCTAGGGATACGCAGTTTGCTCCATGGAGTCTTCGTTATTGTGACATAGTGGCAGGACCATTTTACC";
         final String output = reverseComplement(inputDNA);
-        // you can copy and paste from the terminal window into 
-        //   Project Rosalind to check your answer
         System.out.println(output);
     }
         
-    // This should take the given string and return a String 
-    //   where As and Ts are swapped, Cs and Gs are swapped, 
-    //   and is reversed. 
     private static String reverseComplement(String dnaString) {
-        return "ACCGGGTTTT";
+        StringBuilder result = new StringBuilder();
+        
+        for (char base : dnaString.toCharArray()){
+            if (base == 'A'){
+                result.append ('T');
+            } else if (base == 'T'){
+                result.append ('A');
+            } else if (base == 'C'){
+                result.append ('G');
+            } else if (base == 'G'){
+                result.append ('C');
+            }
+        }
+        return result.reverse (). toString();
     }    
 }
+
